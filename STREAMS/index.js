855c7ec406588e7cefda6c4c81dec9dec9e86565
somyagupta05 <somyagupta052003@gmail.com>
2025-01-15 00:46:19 +0530

    streams

--- STREAMS/index.js
+++ STREAMS/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const zlib = require("zlib");
 const status = require("express-status-monitor");
 
 const app = express();
@@ -7,15 +8,19 @@ const PORT = 8000;
 
 app.use(status());
 
+fs.createReadStream("./sample.txt").pipe(
+  zlib.createGzip().pipe(fs.createWriteStream("./sample.zip"))
+);
+
 app.get("/", (req, res) => {
   fs.readFile("./sample.txt", (err, data) => {
     res.end(data);
   });
 });
 
-app.get("/", (req, res) => {
-  const stream = fs.createReadStream("./sample.txt", "utf-8");
-  stream.on("data", (chunk) => res.write(chunk));
-  stream.on("end", () => res.end());
-});
+// app.get("/", (req, res) => {
+//   const stream = fs.createReadStream("./sample.txt", "utf-8");
+//   stream.on("data", (chunk) => res.write(chunk));
+//   stream.on("end", () => res.end());
+// });
 app.listen(PORT, () => console.log("server started"));
