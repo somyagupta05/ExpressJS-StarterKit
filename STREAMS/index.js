97335feaa78e5ab5408fe4fb5ae4dabdbacb8dfd
somyagupta05 <somyagupta052003@gmail.com>
2025-01-15 00:40:43 +0530

    streams

--- STREAMS/index.js
+++ STREAMS/index.js
@@ -13,4 +13,9 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  const stream = fs.createReadStream("./sample.txt", "utf-8");
+  stream.on("data", (chunk) => res.write(chunk));
+  stream.on("end", () => res.end());
+});
 app.listen(PORT, () => console.log("server started"));
